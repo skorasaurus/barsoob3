67545aaa7bd67b22f8fe358f64742ecec594bab7
Will Skora <skorasaurus@gmail.com>
2011-11-16 15:19:54 -0500

    switched from Envelope to box2d for a boundingbox class, corrected coordinates

--- barsoob3.py
+++ barsoob3.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 import mapnik2
 mapfile = 'barsoob3.xml'
-map_output = 'mapnikxmloutput.png'
-m = mapnik2.Map(800, 600)
+map_output = 'mapnik2xmloutput.png'
+m = mapnik2.Map(400, 400)
 mapnik2.load_map(m, mapfile)
-bbox = mapnik2.Envelope(mapnik.Coord(-81.88, 41.606), mapnik.Coord(-81.5323, 41.3903))
+
+#according to the doc, west,south, east, north
+bbox = mapnik2.Box2d(mapnik2.Coord(-81.738, 41.435), mapnik2.Coord(-81.6723,41.459))
 m.zoom_to_box(bbox) 
 print "Scale = " , m.scale()
 mapnik2.render_to_file(m, map_output) 
