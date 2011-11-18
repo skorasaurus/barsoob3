a68851be3a24123db9276125e92029ddb98e7b18
Will Skora <skorasaurus@gmail.com>
2011-11-18 13:19:09 -0500

    now importing all mapnik2 libaries

--- barsoob3.py
+++ barsoob3.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
-import mapnik2
+from mapnik2 import *
 mapfile = 'barsoob3.xml'
 map_output = 'mapnik2xmloutput.png'
-m = mapnik2.Map(400, 400)
-mapnik2.load_map(m, mapfile)
+m = Map(5200, 5200)
+load_map(m, mapfile)
 
-#according to the doc, west,south, east, north
-bbox = mapnik2.Box2d(mapnik2.Coord(-81.738, 41.435), mapnik2.Coord(-81.6723,41.459))
+
+
+# west,south, east, north
+bbox = Box2d(Coord(-81.738, 41.435),Coord(-81.6723,41.459))
 m.zoom_to_box(bbox) 
 print "Scale = " , m.scale()
-mapnik2.render_to_file(m, map_output) 
 
-
-
+render_to_file(m, map_output) 
